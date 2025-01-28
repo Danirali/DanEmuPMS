2bcc617be10da37de949d1f1cef574263ce64c81
Danirali <74063814+Danirali@users.noreply.github.com>
2025-01-28 22:32:07 +0000

    Update auth_api.py

--- auth_api.py
+++ auth_api.py
@@ -1,11 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_cors import CORS  # Import CORS
 import bcrypt
 import json
 
 app = Flask(__name__)
 
+CORS(app, origins="http://127.0.0.1:8080") 
+
 with open('passwd/credentials.json', 'r') as users_file:
-    users = json.load(users_file) 
+    users = json.load(users_file)
+
+@app.route('/')
+def home_ui():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET'])
+def login_ui():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -17,11 +32,10 @@ def login():
         stored_hashed_password = users[username]['password_hash']
 
         if bcrypt.checkpw(password.encode('utf-8'), stored_hashed_password.encode('utf-8')):
-            return jsonify({"message": "Login successful"}), 200
+            return jsonify({"message": "Login successful", "redirect": "/dashboard"}), 200
         else:
             return jsonify({"message": "Invalid credentials"}), 401
     else:
         return jsonify({"message": "User not found"}), 404
 
-
-app.run(debug=True,port=5000)
+app.run(debug=True, port=8080)
