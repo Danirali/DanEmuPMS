486391875e6483303ac4da73f5e2ca74c021a899
Danirali <74063814+Danirali@users.noreply.github.com>
2025-02-04 08:51:38 +0000

    updated for development v1.1 tag docker

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', async () => {
     loadSection('propertyNav');
+    loadSection('viewPropertyNav');
     fetchProperties('income_property_id');
     fetchProperties('expense_property_id');
+    fetchTables();
+    countProperties();
 
     try {
         const response = await fetch("/api/income/net"); // Replace with your API endpoint
@@ -40,7 +43,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 function loadSection(domain) {
     let navOption = document.getElementById(domain);
     let navOptionLabel = document.getElementById(domain + "Label");
-    navOptions = ['propertyNav', 'incomeNav', 'expensesNav']
+    navOptions = ['propertyNav', 'incomeNav', 'expensesNav', 'viewPropertyNav', 'viewIncomeNav', 'viewExpensesNav']
 
     navOptions.forEach(option => {
         document.getElementById(option).style.display = 'none';
@@ -56,6 +59,8 @@ function loadSection(domain) {
         button.classList.remove('bg-success');
         button.classList.remove('bg-danger');
     });
+
+    fetchTables();
 }
 
 async function fetchProperties(element_id) {
@@ -160,3 +165,115 @@ document.getElementById("expenseForm").addEventListener("submit", async function
         document.getElementById("formMessage").textContent = "Error: Could not add expense.";
     }
 });
+
+async function fetchIncomesTable() {
+    try {
+        const response = await fetch('/api/income');
+        const incomes = await response.json();
+        const tableBody = document.querySelector('#incomesTable tbody');
+        tableBody.innerHTML = '';
+
+        incomes.forEach(income => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${income.name}</td>
+                <td>${income.amount}</td>
+                <td>${income.date}</td>
+                <td>${income.extra}</td>
+                <td><button class="btn btn-danger" onclick="deleteitem(${income.id},'income')">Delete</button></td>
+            `;
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error("Error fetching incomes:", error);
+    }
+}
+
+async function fetchPropertiesTable() {
+    try {
+        const response = await fetch('/api/properties');
+        const properties = await response.json();
+        const tableBody = document.querySelector('#propertiesTable tbody');
+        tableBody.innerHTML = '';
+
+        properties.forEach(property => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${property.name}</td>
+                <td>${property.value}</td>
+                <td>${property.unit}</td>
+                <td><button class="btn btn-danger" onclick="deleteitem(${property.id},'properties')">Delete</button></td>
+            `;
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error("Error fetching properties:", error);
+    }
+}
+
+async function fetchExpensesTable() {
+    try {
+        const response = await fetch('/api/expenses');
+        const expenses = await response.json();
+        const tableBody = document.querySelector('#expensesTable tbody');
+        tableBody.innerHTML = '';
+
+        expenses.forEach(expense => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${expense.name}</td>
+                <td>${expense.amount}</td>
+                <td>${expense.extra}</td>
+                <td><button class="btn btn-danger" onclick="deleteitem(${expense.id},'expenses')">Delete</button></td>
+            `;
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error("Error fetching properties:", error);
+    }
+}
+
+async function deleteitem(id, api) {
+    if (!confirm("Are you sure you want to delete this item?")) return;
+
+    try {
+        const response = await fetch(`/api/${api}/delete/${id}`, {
+            method: 'DELETE'
+        });
+
+        const result = await response.json();
+        alert(result.message);
+
+        if (response.ok) {
+            fetchExpensesTable()
+            fetchIncomesTable()
+            fetchPropertiesTable()
+        }
+    } catch (error) {
+        console.error("Error deleting item:", error);
+        alert("Could not delete item.");
+    }
+}
+
+function fetchTables() {
+    fetchExpensesTable();
+    fetchIncomesTable();
+    fetchPropertiesTable();
+}
+
+async function countProperties() {
+    let response = await fetch("/api/properties");  // Fetch properties from backend
+    let properties = await response.json();
+    let propertyCount = 0;
+    let output = document.getElementById('propertiesCount');
+    let no_data = document.getElementById('propertiesCount_no-data');
+
+    output.innerHTML = 'No Properties';
+
+    properties.forEach(property => {
+        propertyCount = propertyCount + 1
+    });
+
+    no_data.style.display = 'none';
+    output.innerHTML = '<b>' + propertyCount + '</b>'
+}
